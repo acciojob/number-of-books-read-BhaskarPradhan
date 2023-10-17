0a29d9d0d59d69539190603919c29cec21f92d33
BhaskarPradhan <135045424+BhaskarPradhan@users.noreply.github.com>
2023-10-17 10:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,28 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	function numberOfBooksRead(library) {
+  // Check if the library object has a property called 'books' and if it's an array
+  if (library.hasOwnProperty('books') && Array.isArray(library.books)) {
+    // Use the filter method to create a new array containing only read books
+    const readBooks = library.books.filter(book => book.readingStatus === true);
+    
+    // Return the length of the 'readBooks' array, which represents the number of read books
+    return readBooks.length;
+  } else {
+    // If 'books' is not found or not an array, return an error message or handle the situation accordingly
+    return "Invalid library object";
+  }
+}
+
+// Example usage:
+
+  
+
+const numReadBooks = numberOfBooksRead(library);
+console.log("Number of read books: " + numReadBooks);
+
+	
 };
 
 // Do not change the code below
